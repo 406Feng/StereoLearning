86db28eb7b04f74543048e75da7dfe1b9f10d988
wfeng <wangfengrd8868@gmail.com>
2023-03-12 13:52:45 +0800

    注释掉失效语句

--- measurement.py
+++ measurement.py
@@ -114,10 +114,10 @@ cv2.resizeWindow('disp', 600, 600)
 
 cv2.createTrackbar('numDisparities', 'disp', 1, 17, nothing)
 cv2.createTrackbar('blockSize', 'disp', 5, 50, nothing)
-cv2.createTrackbar('preFilterType', 'disp', 1, 1, nothing)
-cv2.createTrackbar('preFilterSize', 'disp', 2, 25, nothing)
+# cv2.createTrackbar('preFilterType', 'disp', 1, 1, nothing)
+# cv2.createTrackbar('preFilterSize', 'disp', 2, 25, nothing)
 cv2.createTrackbar('preFilterCap', 'disp', 5, 62, nothing)
-cv2.createTrackbar('textureThreshold', 'disp', 10, 100, nothing)
+# cv2.createTrackbar('textureThreshold', 'disp', 10, 100, nothing)
 cv2.createTrackbar('uniquenessRatio', 'disp', 15, 100, nothing)
 cv2.createTrackbar('speckleRange', 'disp', 0, 100, nothing)
 cv2.createTrackbar('speckleWindowSize', 'disp', 3, 25, nothing)
@@ -128,10 +128,10 @@ while True:
     # Updating the parameters based on the trackbar positions
     numDisparities = cv2.getTrackbarPos('numDisparities', 'disp') * 16
     blockSize = cv2.getTrackbarPos('blockSize', 'disp') * 2 + 5
-    preFilterType = cv2.getTrackbarPos('preFilterType', 'disp')
-    preFilterSize = cv2.getTrackbarPos('preFilterSize', 'disp') * 2 + 5
+    # preFilterType = cv2.getTrackbarPos('preFilterType', 'disp')
+    # preFilterSize = cv2.getTrackbarPos('preFilterSize', 'disp') * 2 + 5
     preFilterCap = cv2.getTrackbarPos('preFilterCap', 'disp')
-    textureThreshold = cv2.getTrackbarPos('textureThreshold', 'disp')
+    # textureThreshold = cv2.getTrackbarPos('textureThreshold', 'disp')
     uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio', 'disp')
     speckleRange = cv2.getTrackbarPos('speckleRange', 'disp')
     speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize', 'disp') * 2
